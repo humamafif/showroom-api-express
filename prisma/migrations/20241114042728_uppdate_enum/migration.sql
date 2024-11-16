59c698451abdd99244736d3d6bbceead62df52d7
humamafif <afifalmahbubi04@gmail.com>
2024-11-16 19:47:21 +0700

    update .gitignore

--- prisma/migrations/20241114042728_uppdate_enum/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - The values [VirtualAccount] on the enum `PaymentMethod_type` will be removed. If these variants are still used in the database, this will fail.
-
-*/
--- AlterTable
-ALTER TABLE `paymentmethod` MODIFY `type` ENUM('COD', 'VA') NOT NULL;
--- prisma/migrations/20241114042957_uppdate_and_add_enum/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `PaymentMethod` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `type` ENUM('COD', 'VA') NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
